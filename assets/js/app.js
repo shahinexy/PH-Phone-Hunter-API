7d733e521fb996afe999a57643618cc421c8ed31
shahinexy <shahinexy@gmail.com>
2024-02-29 11:50:08 +0600

    show more hide and show work done

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,11 +10,20 @@ function getphoneCard(phones) {
     const cardContainer = document.getElementById('card_container');
     // clear card container before showing new search result.
     cardContainer.textContent = '';
-
     console.log(phones);
+
+    // display show more btn when cards are grater then 12
+    const showMoreBtn = document.getElementById('show_more_btn');
+    if(phones.length > 12){
+        showMoreBtn.classList.remove('hidden')
+    }
+    else{
+        showMoreBtn.classList.add('hidden')
+    }
+
     // display only 12 cards
     phones = phones.slice(0,12);
-    
+
     for (const phone of phones) {
         const div = document.createElement('div');
         div.innerHTML = `
@@ -39,3 +48,11 @@ const searchFild = document.getElementById('search_fild')
 function searchBtn(){
     myData(searchFild.value)
 }
+
+// show more btn
+// const showAll = true;
+// function showMore(){
+//     if(showAll){
+
+//     }
+// }
