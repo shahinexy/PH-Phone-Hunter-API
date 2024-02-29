c787b846ddce55d92126f94c9c5c5a92faf543e8
shahinexy <shahinexy@gmail.com>
2024-02-29 14:14:16 +0600

    project done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const myData = async (searchText, isShowAll) => {
+const myData = async (searchText = '13', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phoens = data.data
@@ -10,7 +10,6 @@ function getphoneCard(phones, isShowAll) {
     const cardContainer = document.getElementById('card_container');
     // clear card container before showing new search result.
     cardContainer.textContent = '';
-    console.log(phones);
 
     // display show more btn when cards are grater then 12
     const showMoreBtn = document.getElementById('show_more_btn');
@@ -37,10 +36,9 @@ function getphoneCard(phones, isShowAll) {
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body text-center">
           <h2 class="text-xl font-semibold">${phone.phone_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <h2 class="text-xl font-semibold">Price</h2>
+          <p>There are many variations of passages of available, but the majority have suffered</p>
           <div class="card-actions justify-center">
-            <button class="btn bg-yellow-500 text-black text-xl font-semibold">Buy Now</button>
+            <button onclick="showDetails('${phone.slug}')" class="btn bg-yellow-500 text-black text-xl font-semibold">Show Details</button>
           </div>
         </div>
       </div>
@@ -51,6 +49,30 @@ function getphoneCard(phones, isShowAll) {
     // end loading time
     loadingTime(false);
 }
+
+// handel card show details btn
+const showDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    showModalDetails(phone)
+}
+// show card modal details
+const showModalDetails = (phone) => {
+    my_modal.showModal();
+    const modalDetails = document.getElementById('modal_details');
+    modalDetails.innerHTML = `
+    <img src="${phone.image}" alt="">
+    <h3 class="font-bold mt-3 text-2xl">${phone.name}</h3>
+    <p class=""> <span class="font-semibold text-xl">Stroge:</span> ${phone.mainFeatures.storage}</p>
+    <p class=""> <span class="font-semibold text-xl">Display Size:</span> ${phone.mainFeatures?.displaySize}</p>
+    <p class=""> <span class="font-semibold text-xl">Chipset:</span> ${phone.mainFeatures?.chipSet}</p>
+    <p class=""> <span class="font-semibold text-xl">Memory:</span> ${phone.mainFeatures?.memory}</p>
+    <p class=""> <span class="font-semibold text-xl">Release Date:</span> ${phone?.releaseDate}</p>
+    <p class=""> <span class="font-semibold text-xl">GPS:</span> ${phone?.others?.GPS || 'No GPS'}</p>
+    `
+}
+
 
 // phone search fild
 const searchFild = document.getElementById('search_fild')
@@ -74,4 +96,12 @@ function loadingTime(isload){
 // show All btn
 function showAll(){
     searchBtn(true);
+}
+
+myData()
+
+function setInnerText(id, value){
+    const result = document.getElementById(id).innerText;
+    result = value;
+    return result
 }
